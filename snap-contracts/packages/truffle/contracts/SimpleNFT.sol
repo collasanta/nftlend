a77cee5612b7025f2328aabff76f4ee090ab76c8
Victor Collasanta <victorcollasanta@Victors-MacBook-Pro.local>
2024-02-17 08:47:39 -0300

    add snaps/contract to frontend

--- snap-contracts/packages/truffle/contracts/SimpleNFT.sol
+++ snap-contracts/packages/truffle/contracts/SimpleNFT.sol
@@ -3,14 +3,11 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract ERC721Mock is ERC721 {
+
+
+contract SimpleNFT is ERC721 {
     constructor() ERC721("Mock NFT", "MNFT") {}
-
-    function mint(address to, uint256 tokenId) public {
-        _mint(to, tokenId);
-    }
-
-    function burn(uint256 tokenId) public {
-        _burn(tokenId);
+    function mint(uint256 tokenId) public {
+        _mint(msg.sender, tokenId);
     }
 }
