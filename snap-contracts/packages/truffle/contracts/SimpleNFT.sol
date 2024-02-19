5d006a3ea0d8e68d063911c6053068155c8b3efe
Victor Collasanta <victorcollasanta@Victors-MacBook-Pro.local>
2024-02-19 09:11:33 -0300

    Clean up undesired MetaMask GitHub files

--- snap-contracts/packages/truffle/contracts/SimpleNFT.sol
+++ snap-contracts/packages/truffle/contracts/SimpleNFT.sol
@@ -3,8 +3,6 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-
-
 contract SimpleNFT is ERC721 {
     constructor() ERC721("Mock NFT", "MNFT") {}
     function mint(uint256 tokenId) public {
