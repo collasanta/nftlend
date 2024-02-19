2fd94fc2d99dd6e2e822688934216b16db3a061b
Victor Collasanta <victorcollasanta@Victors-MacBook-Pro.local>
2024-02-19 13:59:41 -0300

    add CI/CD azure + chainID Handling

--- nest-react/apps/client/src/lib/utils.ts
+++ nest-react/apps/client/src/lib/utils.ts
@@ -10,10 +10,56 @@ export type TransactionParams = {
 
 export type EthereumTransactionParams = TransactionParams[] | [string, string] | object; 
 
+// export const requestAccounts = async (): Promise<string[]> => {
+//   const accounts = await ethereumRequest({ method: 'eth_requestAccounts' });
+//   return accounts as string[];
+// };
+
 export const requestAccounts = async (): Promise<string[]> => {
-  const accounts = await ethereumRequest({ method: 'eth_requestAccounts' });
-  return accounts as string[];
-};
+  // Check if Ethereum provider is injected
+  if (window.ethereum) {
+      try {
+          const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+          const chainId = await window.ethereum.request({ method: 'eth_chainId' });
+
+          const lineaGoerliChainId = '0xe704';
+
+          if (chainId !== lineaGoerliChainId) {
+              const wasSwitchSuccessful = await window.ethereum.request({
+                  method: 'wallet_addEthereumChain',
+                  params: [{
+                      chainId: lineaGoerliChainId,
+                      chainName: 'Linea Testnet',
+                      nativeCurrency: {
+                          name: 'ETH',
+                          symbol: 'ETH',
+                          decimals: 18
+                      },
+                      rpcUrls: ['https://rpc.goerli.linea.build'],
+                      blockExplorerUrls: ['https://goerli.lineascan.build']
+                  }]
+              });
+
+              if (wasSwitchSuccessful) {
+                  console.log('Switched to linea-goerli network');
+              } else {
+                  console.error('Failed to switch to linea-goerli network');
+              }
+          }
+
+          return accounts as string[];
+      } catch (error) {
+          console.error(error);
+          window.alert('Failed to connect to MetaMask');
+          return [];
+      }
+  } else {
+      console.log('Please install MetaMask!');
+      window.alert('Please install MetaMask!');
+      return [];
+  }
+}
 
 export const ethereumRequest = async (request: { method: string; params?: EthereumTransactionParams }) => {
   if (window.ethereum) {
