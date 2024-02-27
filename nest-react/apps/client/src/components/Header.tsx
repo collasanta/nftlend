57fb9122046cc7dcbce18702cdaa8cc3600dc206
Victor Collasanta <victorcollasanta@Victors-MacBook-Pro.local>
2024-02-27 10:41:40 -0300

    fix docker deployment

--- nest-react/apps/client/src/components/Header.tsx
+++ nest-react/apps/client/src/components/Header.tsx
@@ -6,8 +6,6 @@ export const Header = () => {
       <div className='flex flex-row items-center space-y-4 sm:space-y-0 justify-between max-w-[1050px] px-4 mx-auto py-2 sm:py-5'>
         <span className='font-bold  rounded-lg p-3 items-center mt-4 sm:mt-0 text-[12px] sm:text-[25px]'>💰 NFTLend 🖼️</span>
         <ConnectButton />
-        {/* <SignButton />
-        <InstallSnapButton /> */}
       </div>
     </div>
   )
